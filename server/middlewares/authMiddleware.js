cda916c669f03be65d5b34254b5d9de5a247fdc4
Belén Robledo <beeelrobledo@gmail.com>
2023-03-20 02:31:44 -0300

    delete debug logs

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -4,16 +4,11 @@ const { setTokenCookies, validateToken, refreshTokens } = require('../utils/auth
 async function authenticate(req, res, next) {
     try {
         const { accessToken, refreshToken } = parseCookies(req.headers.cookie);
-        console.log("TOKENS: ", accessToken, refreshToken);
-
-        //const {userId, userRole} = await validateToken(accessToken);
         const accessTokenData = await validateToken(accessToken);
-        console.log("accessTokenData:", accessTokenData);
 
         if(!accessTokenData){
             //accessToken isn't valid - check refreshToken
             const refreshTokenData = await validateToken(refreshToken);
-            console.log("refreshTokenData:", refreshTokenData);
 
             if(!refreshTokenData){
                 //refreshToken isn't valid - 401 unauthorized and redirect.
@@ -22,12 +17,9 @@ async function authenticate(req, res, next) {
                 //refreshToken is valid
                 const {userId, userRole} = refreshTokenData;
                 const newTokens = refreshTokens(refreshToken, userId, userRole);
+
                 if(newTokens){
                     res = setTokenCookies(res, newTokens);
-
-                    console.log("userID: ", userId);
-                    console.log("userRole: ", userRole);
-
                     next();
                 } else {
                     //error generating new tokens
@@ -36,9 +28,6 @@ async function authenticate(req, res, next) {
             }
         } else {
             const {userId, userRole} = accessTokenData;
-            console.log("userID: ", userId);
-            console.log("userRole: ", userRole);
-
             next();
         }
     } catch (err) {
